825d95c08436c359865e0bddbeb761581d9d7c8c
Fabius-Naumann <fabius.naumann@icloud.com>
2026-01-09 22:36:47 +0100

    add project description, update workflows to use 'uv' for Python setup and dependency management and run linters

--- src/sign_ml/train.py
+++ src/sign_ml/train.py
@@ -1,10 +1,12 @@
-from sign_ml.model import Model
 from sign_ml.data import MyDataset
+from sign_ml.model import Model
+
 
 def train():
     dataset = MyDataset("data/raw")
     model = Model()
     # add rest of your training code here
+
 
 if __name__ == "__main__":
     train()
