bec14234b57d73f855198a31e41a1ff256951615
Fabius-Naumann <fabius.naumann@icloud.com>
2026-01-09 18:58:53 +0100

    init cookiecutter project and update dependencies

---
+++ src/sign_ml/train.py
@@ -0,0 +1,10 @@
+from sign_ml.model import Model
+from sign_ml.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
